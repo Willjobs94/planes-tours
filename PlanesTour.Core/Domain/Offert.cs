baebf268ba8bde45a9759477222a84475aa4efdd
Willam Bello <william_bm@hotmail.es>
2016-02-14 18:18:28 -0400

    finish Offert.cs

--- PlanesTour.Core/Domain/Offert.cs
+++ PlanesTour.Core/Domain/Offert.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,35 @@ namespace PlanesTour.Core.Domain
 {
     class Offert : EntityBase
     {
+        /// <summary>
+        /// Hotel in which the offer applies 
+        /// </summary>
+        public int HotelId { get; set; }
 
+        /// <summary>
+        /// A short description of the offert
+        /// </summary>
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Indicates when the offer start
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Indicates when the Offert Expire
+        /// </summary>
+        public DateTime ExpirationDate { get; set; }
+
+        /// <summary>
+        /// How many cuppuns does this offer have
+        /// </summary>
+        public int OffertAmount { get; set; }
+
+        /// <summary>
+        /// Indicate is the offer is active
+        /// </summary>
+        public bool IsActive { get; set; }
     }
 }

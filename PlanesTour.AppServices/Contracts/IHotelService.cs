8ea0c29697c8988476d1c6fd83fe74456eaec96a
Willam Bello <william_bm@hotmail.es>
2016-02-16 20:47:39 -0400

    add ClassLibrary AppServices

---
+++ PlanesTour.AppServices/Contracts/IHotelService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanesTour.Repository.Contracts;
+using PlanesTour.Core.Domain;
+
+namespace PlanesTour.AppServices.Contracts
+{
+    public interface IHotelService
+    {
+        void CreateNewHotel(Hotel hotel);
+        void UpdateViewCount(int hotelId);
+
+        List<Hotel> GetAllHotels();
+        List<Hotel> GetAllHotelsByLocationDescendent();
+    }
+}
---
+++ PlanesTour.AppServices/HotelService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlanesTour.AppServices.Contracts;
+using PlanesTour.Repository.Contracts;
+using PlanesTour.Core.Domain;
+
+namespace PlanesTour.AppServices
+{
+    public class HotelService : IHotelService
+    {
+
+        public HotelService(IHotelRepository hotelRespository)
+        {
+            _hotelRepository = hotelRespository;
+        }
+
+        public void CreateNewHotel(Hotel hotel)
+        {
+            _hotelRepository.Add(hotel);
+            _hotelRepository.SaveChanges();
+        }
+
+        public List<Hotel>GetAllHotels()
+        {
+           return _hotelRepository.GetAllHotels();
+        }
+
+        public void UpdateViewCount(int hotelId)
+        {
+            var hotel = _hotelRepository.GetHotelById(hotelId);
+
+            if (hotel == null) return;
+            hotel.ViewCount++;
+            _hotelRepository.SaveChanges(); 
+        }
+
+        public List<Hotel> GetAllHotelsByLocationDescendent()
+        {
+            return _hotelRepository.GetHotelsByLocation().ToList();
+        }
+        private readonly IHotelRepository _hotelRepository;
+    }
+}

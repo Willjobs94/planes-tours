d6894bf4cfa5af8a0999bb1874c6b2eb36466f41
Willam Bello <william_bm@hotmail.es>
2016-02-17 20:20:46 -0400

    Issue of DI on HomeController.cs

--- PlanesTour.Data/Migrations/Configuration.cs
+++ PlanesTour.Data/Migrations/Configuration.cs
@@ -15,18 +15,7 @@ namespace PlanesTour.Data.Migrations
 
         protected override void Seed(PlanesTour.Data.PlanesTourDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
             #region Location List
             var locationList = new List<Location>
             {
